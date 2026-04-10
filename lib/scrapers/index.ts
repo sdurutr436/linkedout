149c2eecca69b5bfbc29cffbd220f3c443ff6e63
sdurutr436 <sdurutr436@g.educaand.es>
2026-04-10 03:00:27 +0200

    feat(scrapers): add modular LinkedIn and Infojobs Playwright scrapers

---
+++ lib/scrapers/index.ts
@@ -0,0 +1,3 @@
+export { linkedInScraper } from "./linkedin";
+export { infojobsScraper } from "./infojobs";
+export type { JobListing, ScraperCredentials, SearchParams, JobScraper } from "./types";
